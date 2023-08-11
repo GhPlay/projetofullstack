c6a02f86bdfbae7825fff67d3b4552c362c08bec
Marçal <byelmarcal@hotmail.com>
2023-08-11 17:35:31 -0300

    first commit

---
+++ src/main/java/com/marcal/crudbackend/domain/controller/EmployeeController.java
@@ -0,0 +1,26 @@
+package com.marcal.crudbackend.domain.controller;
+
+import com.marcal.crudbackend.domain.model.Employee;
+import com.marcal.crudbackend.domain.repositories.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/api/v1")
+public class EmployeeController {
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @GetMapping
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+
+
+
+}
---
+++ src/main/java/com/marcal/crudbackend/domain/model/Employee.java
@@ -0,0 +1,23 @@
+package com.marcal.crudbackend.domain.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Entity
+@Table(name = "employees")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String first_name;
+    private String last_name;
+    private String emailId;
+}
